c9fd939f055edc50823c0d106e981f7d269d2506
niomicsam <biskuitkelapa@gmail.com>
2020-05-11 09:34:17 +0700

    Tugas 21 JS
    Buatlah perubahan kata “masak” menjadi “makan” dalam kalimat “Saya belajar masak nasi goreng” sehingga akan tampil seperti gambar berikut ini :

---
+++ tugas21.js
@@ -0,0 +1,7 @@
+function isi(){
+  var cari = "Saya belajar masak nasi goreng";
+  console.log(cari);
+  // var cari = "Saya belajar masak nasi goreng";
+  console.log(cari.replace("masak","makan"))
+}
+isi();
